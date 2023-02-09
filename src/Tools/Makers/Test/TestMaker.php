10069f524e750db200c2ff7c1f97fa4eeb81c15c
Homero Raul <h.raulvc@gmail.com>
2023-02-08 23:08:50 -0600

    Test Update

--- src/Tools/Makers/Test/TestMaker.php
+++ src/Tools/Makers/Test/TestMaker.php
@@ -12,10 +12,32 @@ class TestMaker extends MakerTool
 
 	protected $testTemplatePath;
 
+	protected $testCasePath;
+
+	protected $testUnitPath;
+
 	private function setTestPath()
 	{
 
 		$this->testPath = get_path('tests/Feature/Models');
+
+		return $this;
+
+	}
+
+	private function setTestCasePath()
+	{
+
+		$this->testCasePath = get_path('tests');
+
+		return $this;
+
+	}
+
+	private function setTestUnitPath()
+	{
+
+		$this->testUnitPath = get_path('tests/Unit');
 
 		return $this;
 
@@ -35,7 +57,96 @@ class TestMaker extends MakerTool
 
 		$this->init($ModelName)
 			->setTestPath()
-			->setTestTemplatePath();
+			->setTestCasePath()
+			->setTestUnitPath()
+			->setTestTemplatePath()
+			->createTestCaseClass()
+			->createPhpUnitXmlFile()
+			->addTestNamespaceToComposerJson()
+			->createFeatureTest();
+
+	}
+
+	private function createTestCaseClass()
+	{
+
+		$testCaseFile = $this->testCasePath . '/TestCase.php';
+
+		if(!file_exists($testCaseFile)) {
+
+			$templateFile = $this->testTemplatePath . '/TestCaseTemplate.txt';
+
+			if(copy($templateFile, $testCaseFile)) {
+
+				$this->replaceData($testCaseFile);
+
+			}
+
+		}
+
+		return $this;
+
+	}
+
+	private function createPhpUnitXmlFile()
+	{
+
+		$phpunitFile = root_path() . '/phpunit.xml';
+
+		if(!file_exists($phpunitFile)) {
+
+			$templateFile = $this->testTemplatePath . '/PhpunitTemplate.txt';
+
+			copy($templateFile, $phpunitFile);
+
+		}
+
+		return $this;
+
+	}
+
+	private function addTestNamespaceToComposerJson()
+	{
+
+		if(app_dir_name() == 'src') {
+
+			$composerJsonPath = root_path() . '/composer.json';
+
+		    $composerJsonData = json_decode(file_get_contents($composerJsonPath), true);
+
+			$baseNamespace = array_keys($composerJsonData['autoload']['psr-4'])[0];
+
+			if (isset($composerJsonData['autoload-dev']['psr-4'])) {
+
+			    $composerJsonData['autoload-dev']['psr-4'][$baseNamespace . 'Tests\\'] = 'tests/';
+
+			} else {
+			    
+			    $composerJsonData['autoload-dev'] = [
+			    
+			        'psr-4' => [
+			    
+			            $baseNamespace . 'Tests\\' => 'tests/',
+			    
+			        ],
+			    
+			    ];
+
+			}
+
+			file_put_contents(
+				$composerJsonPath, 
+				json_encode($composerJsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+			);
+
+		}
+
+		return $this;
+
+	}
+
+	private function createFeatureTest()
+	{
 
 		$testFile = $this->testPath . '/' . $this->PascalCaseModelName . 'EndpointsTest.php';
 
@@ -60,7 +171,8 @@ class TestMaker extends MakerTool
 		}
 
 		return true;
-
+		
 	}
+
 
 }
